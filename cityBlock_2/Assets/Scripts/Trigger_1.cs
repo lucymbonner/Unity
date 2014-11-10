6271bf5aae56065e868ddd229617294cba503083
lucymbonner <lucybonner@Lucys-MacBook-Pro-2.local>
2014-11-10 18:29:11 -0500

    with man

---
+++ cityBlock_2/Assets/Scripts/Trigger_1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger_1 : MonoBehaviour {
+
+	public AudioClip Explosion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter() {
+		Debug.Log ("Trigger fired");
+
+		audio.PlayOneShot (Explosion);
+	}
+}
